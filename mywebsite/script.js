a1e58b9173a0e7f3a4db046cf9a1463e533c0b4d
Shafee Ahmad <152318336+SHAFEE12@users.noreply.github.com>
2026-02-15 17:26:14 +0530

    Enhance interactivity with audio and camera features

--- mywebsite/script.js
+++ mywebsite/script.js
@@ -1,3 +1,4 @@
+
 document.addEventListener("DOMContentLoaded", () => {
 
     const video = document.getElementById("myVideo");
@@ -7,8 +8,93 @@ document.addEventListener("DOMContentLoaded", () => {
         video.play();
     }
 
+ 
     document.addEventListener("click", enableSound, { once: true });
     document.addEventListener("touchstart", enableSound, { once: true });
+
+});
+
+let yes = document.getElementById("yes");
+let message = document.getElementById("message");
+let audio = document.getElementById("myaudio");
+let no = document.getElementById("no");
+let container = document.querySelector(".container");
+
+
+
+
+
+no.addEventListener("mouseover", () => {
+
+    let containerWidth = container.clientWidth;
+    let containerHeight = container.clientHeight;
+
+    let buttonWidth = no.offsetWidth;
+    let buttonHeight = no.offsetHeight;
+
+    let randomX = Math.random() * (containerWidth - buttonWidth);
+    let randomY = Math.random() * (containerHeight - buttonHeight);
+
+    no.style.left = randomX + "px";
+    no.style.top = randomY + "px";
+});
+
+yes.addEventListener("click", () => {
+    
+    message.innerText = "I love you too 💖";
+
+
+    setTimeout(() => {
+        message.innerText = "Wait... I want to say something ❤️";
+    }, 2000);
+
+
+    setTimeout(() => {
+        audio.play();
+    }, 4000);
+
+});
+
+
+let video = document.getElementById("myVideo");
+
+yes.addEventListener("click", () => {
+    video.pause();   
+});
+
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    const openCamBtn = document.getElementById("openCam");
+    const cameraVideo = document.getElementById("camera");
+    const questionText = document.getElementById("question");
+        const bgVideo = document.getElementById("myVideo");
+
+    openCamBtn.addEventListener("click", async function () {
+
+             if (myVideo) {
+            bgVideo.pause();
+            bgVideo.currentTime = 0;
+        }
+
+        try {
+            const stream = await navigator.mediaDevices.getUserMedia({
+                video: true
+            });
+
+            cameraVideo.srcObject = stream;
+            cameraVideo.style.display = "block";
+
+       
+            setTimeout(() => {
+                questionText.innerText = "See? Chand bhi sharma jaaye 😍💖";
+            }, 2000);
+
+        } catch (error) {
+            questionText.innerText = "Camera permission denied 😢";
+        }
+
+    });
 
 });
 
